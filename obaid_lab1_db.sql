2cbbbcc2c970917a01fc2a9de32c250008afa4d4
obaidullahnaeem <obaidullahnaeem55@gmail.com>
2026-09-06 22:00:38 +0500

    first lab of db , done

---
+++ obaid_lab1_db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE obaid_281147487 ;
+Use obaid_281147487 ;
+
+-- only crete columbs 
+CREATE TABLE Student
+(
+PersonID varchar(255),
+LastName varchar(255),
+FirstName varchar(255),
+Age varchar(255),
+City varchar(255)
+);
+
+Select * From Student ;
+
+
+INSERT INTO Student VALUES
+('001', 'Li', 'Mi', '10', 'Manga'),
+('002', 'Moo', 'Toe', '23', 'Up'),
+('003', 'Faah', 'Poopy', '20', 'Netherland'),
+('006', 'Svendson', 'Tove', 'Borgvn 23', 'Sandnes'),
+('004', 'Petterson', 'Kari', 'Strogt 20', 'Stavanger')
+;
+truncate table Student ; -- to delete the all values
